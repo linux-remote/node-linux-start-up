144e96c4bfa15c2fb85d03d192cc2a9ac7c380af
dw <tianxiazhizhong@gmail.com>
2019-03-01 19:39:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const rawFilePath = '/etc/rc.d/rc.local';
-const lrConfigFilePath = path.join(__dirname, 'config.sh');
-const content = '// LR-N';
-function init() {
+const note = `// linux-remote start-up`;
+const childProcess = require('child_process');
+function init(confJsPath) {
     
     const raw = fs.readFileSync(rawFilePath, 'utf-8');
     if(raw.indexOf(content) === -1) {
         fs.writeFileSync(lrConfigFilePath);
+        let cmd = process.argv[0] + ' ' + confJsPath;
+        const content = note + '\n' + cmd;
         fs.appendFileSync(rawFilePath, content, 'utf-8');
+        childProcess.execSync(cmd);
     }
 };
 
